a83ce8eedb9d07e8b8ed0955d18299ce63408208
Yu <112357579+ynarafu@users.noreply.github.com>
2024-11-07 12:35:43 +0900

    fix testcode

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -63,15 +63,4 @@ class UserTest < ActiveSupport::TestCase
     @user.password = @user.password_confirmation = "a" * 5
     assert_not @user.valid?
   end
-
-  test "valid signup information" do
-    assert_difference 'User.count', 1 do
-      post users_path, params: { user: { name:  "Example User",
-                                         email: "user@example.com",
-                                         password:              "password",
-                                         password_confirmation: "password" } }
-    end
-    follow_redirect!
-    assert_template 'users/show'
-  end
 end
